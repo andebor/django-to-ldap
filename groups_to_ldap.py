f03f8bab12c6a0cac0f9dab6c9c7b3c542489c72
Anders Borud <post@andersborud.com>
2015-07-05 19:36:27 +0000

    Added script for adding users to groups.

---
+++ groups_to_ldap.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, sys, django, json
+
+# Get config from json
+with open('config.json') as config:
+    config = json.load(config)
+project_dir = config['project_dir']
+project_name = config['project_name']
+
+# Connect to django models
+sys.path.append(project_dir)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", project_name + ".settings")
+from django.conf import settings
+django.setup()
+from django.contrib.auth.models import User, Group
+
+groups = Group.objects.all()
+complete_string = u""
+
+for group in groups:
+    users = group.user_set.all()
+    for user in users:
+        ldif_string = u"""
+dn: cn={groupname},ou=groups,dc=vm,dc=borud,dc=eu
+changetype: modify
+add: member
+member: {username},ou=people,dc=vm,dc=borud,dc=eu
+"""
+        d = {
+            'groupname': group.name,
+            'username': user.username
+            }
+        formatted_string = ldif_string.format(**d)
+        complete_string += formatted_string
+
+with open("add_users_to_groups.ldif", "w") as output_file:
+    output_file.write(complete_string.encode('utf-8'))
